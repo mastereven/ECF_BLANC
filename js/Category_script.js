febee2d77c8fb37ce062be7541033c4158bd6034
mastereven <80420807+mastereven@users.noreply.github.com>
2024-03-15 15:23:50 +0100

    Search by Categories javascript part is done
    Fin de la partie sur la recherche par catégories et commencement de recherche par ordre alphabétique.

---
+++ js/Category_script.js
@@ -0,0 +1,27 @@
+async function GetUrlCategory() {
+  const cat = new URLSearchParams(document.location.search).get("c");
+  let CategoryUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${cat}`;
+  let xhr = fetch(CategoryUrl)
+    .then(function (promise) {
+      return promise.json();
+    })
+    .then(function (SuccessCategory) {
+      let htmlCorrectCat = document.getElementById("ChoosenCategory");
+      for (i = 0; i < SuccessCategory.meals.length - 1; i++) {
+        let plateId = SuccessCategory.meals[i].idMeal;
+        let plateName = SuccessCategory.meals[i].strMeal;
+        let plateImgSrc = SuccessCategory.meals[i].strMealThumb;
+        htmlCorrectCat.innerHTML += `
+        <a href="Recipe.html?id=${plateId}">
+                <div class="tagsFood">
+                    <p><img src="${plateImgSrc}" alt="${plateName} width="150" height="150"><br>${plateName}</p>
+                </div>
+            </a>`;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+GetUrlCategory();
---
+++ js/recipe_script.js
@@ -0,0 +1,42 @@
+async function GetRecipe() {
+  const cat = new URLSearchParams(document.location.search).get("id");
+  let RecipeUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${cat}`;
+
+  let xhr = fetch(RecipeUrl)
+    .then(function (promise) {
+      return promise.json();
+    })
+    .then(function (SuccessRecipe) {
+      let htmlRecipePart = document.getElementById("ChoosenCategory");
+
+      for (let c of SuccessRecipe.meals) {
+        let plateName = c.strMeal;
+        let plateCat = c.strCategory;
+        let plateOrigin = c.strArea;
+        let plateIngredientMeasure = [];
+        for (let i = 1; i < 21; i++) {
+            if(!c[`strIngredient${i}`]) break;
+            let ingredient = c[`strIngredient${i}`];
+            let measure = c[`strMeasure${i}`];
+            let ingredientMeasure = `${ingredient}` + ` ` + `${measure}`;
+            console.log(ingredientMeasure);
+            if (ingredientMeasure) {
+                plateIngredientMeasure.push(ingredientMeasure);
+            }
+        }
+        htmlRecipePart.innerHTML += `
+          <div class="Recipe">
+            <h1>${plateName}</h1><br>
+            <h2>Catégorie : ${plateCat}<br>
+            Zone géographique : ${plateOrigin}<br><br> 
+            </h2>
+            <h1>Ingrédients:<br><br>- ${plateIngredientMeasure.join(`.<br>-`)}</h1>
+          </div>`;
+      }
+    
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+GetRecipe();
